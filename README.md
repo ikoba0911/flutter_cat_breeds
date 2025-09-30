f9204430129cdfebd935337bc2e6dc928abcdf62
ikoba0911 <ikki.5884.dev@gmail.com>
2025-09-30 20:42:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_cat_breeds
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/catbreeds/flutter_cat_breeds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.catbreeds.flutter_cat_breeds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
