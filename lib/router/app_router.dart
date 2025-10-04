5de2935a9d4527317a0aaf68c9728af393097c1d
ikoba0911 <ikki.5884.dev@gmail.com>
2025-10-04 22:44:54 +0900

    不要なnavigatorKeyの削除

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cat_breeds/router/app_route_name.dart';
 import 'package:flutter_cat_breeds/view/home_screen.dart';
 
-// App global navigator key.
-// Used to return from the bottom sheet to the root navigator.
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
 final class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     Widget builder;
